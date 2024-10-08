07d9ed6aa950939ed6c5e73825b21045cfc542a5
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-08 11:32:27 +0100

    sprint

--- starter/src/ThemeToggle.jsx
+++ starter/src/ThemeToggle.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useGlobalContext } from "./context";
 
 const ThemeToggle = () => {
+  const { greeting } = useGlobalContext();
+  console.log(greeting);
   return <h2>ThemeToggle</h2>;
 };
 
