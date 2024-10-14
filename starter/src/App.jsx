ac4aaa0a6efd3fc2f946beb19af46f14ff66cb85
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-14 15:06:43 +0100

    sprint

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -2,8 +2,6 @@ import Gallery from "./Gallery";
 import SearchForm from "./SearchForm";
 import ThemeToggle from "./ThemeToggle";
 
-// const myAccessKey(API_KEY)= 'u5-cS902fIoqpYNrmyh6ZSsoBnjVLXtqp_s5Bs_Dlhw'
-
 const App = () => {
   return (
     <main>
--- starter/src/Gallery.jsx
+++ starter/src/Gallery.jsx
@@ -1,6 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
-
+const url =
+  "https://api.unsplash.com/search/photos?client_id=u5-cS902fIoqpYNrmyh6ZSsoBnjVLXtqp_s5Bs_Dlhw&query=office";
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  console.log(response);
   return <h2>Gallery</h2>;
 };
 
