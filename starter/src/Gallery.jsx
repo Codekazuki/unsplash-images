637818aa2e1502c04477fba6b83a21538c3badd6
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-14 16:05:23 +0100

    sprint

--- starter/src/Gallery.jsx
+++ starter/src/Gallery.jsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import axios from "axios";
 const url =
-  "https://api.unsplash.com/search/photos?client_id=u5-cS902fIoqpYNrmyh6ZSsoBnjVLXtqp_s5Bs_Dlhw&query=office";
+  "https://api.unsplash.com/search/photos?client_id=u5-cS902fIoqpYNrmyh6ZSsoBnjVLXtqp_s5Bs_Dlhw&query=king";
 const Gallery = () => {
   const response = useQuery({
     queryKey: ["images"],
@@ -10,8 +11,43 @@ const Gallery = () => {
       return result.data;
     },
   });
-  console.log(response);
-  return <h2>Gallery</h2>;
+  if (response.isLoading) {
+    return (
+      <section className='image-container'>
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className='image-container'>
+        <h4>Error occured</h4>
+      </section>
+    );
+  }
+  const results = response.data.results;
+  if (results.length < 1) {
+    return (
+      <section className='image-container'>
+        <h4>No result found</h4>
+      </section>
+    );
+  }
+  return (
+    <section className='image-container'>
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            alt={item.alt_description}
+            className='img'
+            key={item.id}
+          />
+        );
+      })}
+    </section>
+  );
 };
 
 export default Gallery;
