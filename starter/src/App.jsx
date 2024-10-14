80e4fb31706a0ccdfdd61bdee4defdb0f3ffb4f4
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-14 14:58:56 +0100

    sprint

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -2,6 +2,8 @@ import Gallery from "./Gallery";
 import SearchForm from "./SearchForm";
 import ThemeToggle from "./ThemeToggle";
 
+// const myAccessKey(API_KEY)= 'u5-cS902fIoqpYNrmyh6ZSsoBnjVLXtqp_s5Bs_Dlhw'
+
 const App = () => {
   return (
     <main>
