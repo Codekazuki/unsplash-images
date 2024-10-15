9ed4f6eeed9f86730320be0437c0ff06e7558697
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-15 11:10:50 +0100

    almost

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,9 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import axios from "axios";
 import { useGlobalContext } from "./context";
-const url = `https://api.unsplash.com/search/photos?client_id=${
-  import.meta.env.VITE_API_KEY
-}`;
+const url = `https://api.unsplash.com/search/photos?client_id=
+u5-cS902fIoqpYNrmyh6ZSsoBnjVLXtqp_s5Bs_Dlhw`;
 const Gallery = () => {
   const { searchTerm } = useGlobalContext();
   const response = useQuery({
